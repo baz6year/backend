ed0e57cc254eaa7c95dc211acb9fdcaa62750722
alimanbazarbekova <aliman.bazarbekova@alatoo.edu.kg>
2025-05-05 00:56:49 +0600

    authentication controller

---
+++ src/main/java/com/example/ArtGallery/controller/AuthController.java
@@ -0,0 +1,28 @@
+package com.example.ArtGallery.controller;
+
+import com.example.ArtGallery.dto.AuthResponse;
+import com.example.ArtGallery.dto.LoginRequest;
+import com.example.ArtGallery.dto.RegisterRequest;
+import com.example.ArtGallery.service.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/auth")
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public AuthResponse register(@RequestBody RegisterRequest registerRequest) {
+        return authService.register(registerRequest);
+    }
+
+    @PostMapping("/login")
+    public AuthResponse login(@RequestBody LoginRequest loginRequest) {
+        return authService.login(loginRequest);
+    }
+}
