915f8752574e4b8ea5c6f654c8e52612314589a0
alimanbazarbekova <aliman.bazarbekova@alatoo.edu.kg>
2025-05-05 00:56:23 +0600

    user controller improvement

--- src/main/java/com/example/ArtGallery/controller/UserController.java
+++ src/main/java/com/example/ArtGallery/controller/UserController.java
@@ -1,17 +1,20 @@
 package com.example.ArtGallery.controller;
 
 import com.example.ArtGallery.dto.UserRequest;
+import com.example.ArtGallery.dto.UserResponse;
 import com.example.ArtGallery.model.User;
 import com.example.ArtGallery.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/user")
 public class UserController {
     private final UserService userService;
-    @GetMapping("/get/{id}")
+    /* @GetMapping("/get/{id}")
     public User getUserById(@RequestParam("id") Long id) {
         return userService.getUserById(id);
     }
@@ -26,5 +29,20 @@ public class UserController {
     @DeleteMapping("/delete")
     public void deleteUser(Long id){
         userService.deleteUser(id);
+    } */
+    @PostMapping("/create")
+    public UserResponse createUser(@RequestBody UserRequest userRequest) {
+        return userService.createUser(userRequest);
+    }
+
+    @GetMapping("/all")
+    public List<UserResponse> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/get-email")
+    public UserResponse getByEmail(@RequestParam String email) {
+        return userService.getByEmail(email);
+
     }
 }
